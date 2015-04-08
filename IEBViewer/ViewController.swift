3dc8f46041f346261c0e2938c1172a62c2030a3f
Miae Lee <malee@shou.kr>
2015-04-08 14:57:58 +0900

    epub zip 분해 후 entry 목록 출력

--- IEBViewer/ViewController.swift
+++ IEBViewer/ViewController.swift
@@ -12,16 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-
+        
+        
+        // Get epub
+        let ebook_path = NSBundle.mainBundle().pathForResource("aa", ofType: "epub")
+    
+        var ebook_path_url = NSURL.fileURLWithPath(ebook_path!)
+        
+        if ((ebook_path_url) != nil) {
+            ebook_unzip(ebook_path_url!)
+        } else {
+            NSLog("Can't Not found ebook_path_url!")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func ebook_unzip(ebook_path_url : NSURL) {
+        NSLog("ebook_unzip method start!");
+        
+        let archive: ZZArchive = ZZArchive(URL:(fileURLWithPath:ebook_path_url), error: nil)
+        for ebook_entry in archive.entries {
+            let ebook_entry_info : ZZArchiveEntry = ebook_entry as ZZArchiveEntry
+            
+             NSLog("Unzip ebook Entry : \(ebook_entry_info.fileName)")
+        }
+    }
 }
-
